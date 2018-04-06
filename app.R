d71aa64006f2863d62d904b58a9759450b550445
Aditya Bhati <33009537+adityabhati12@users.noreply.github.com>
2018-04-06 02:15:09 +0100

    commit3- dashboard and plotly added 4-6-2018

--- app.R
+++ app.R
@@ -6,15 +6,18 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+library(plotly)
 library(shiny)
 library(DT)
 library(ggplot2)
 library(htmltools)
 library(dplyr)
 library(shinydashboard)
+library(googleVis)
 
-emp <-read.csv("D:/aditya/TRINITY/Data Visualization/WA_Fn-UseC_-HR-Employee-Attrition.csv")
+emp <-
+  read.csv("D:/aditya/TRINITY/Data Visualization/WA_Fn-UseC_-HR-Employee-Attrition.csv")
+
 
 n_rows <- as.numeric(nrow(emp))
 all_Jobroles <- sort(unique(emp$JobRole))
@@ -41,89 +44,115 @@ emp$PercentSalaryHike <- NULL
 # Define UI for application that plots features of movies
 ui <- dashboardPage(
   dashboardHeader(title = "Analysis of Employee Attrition"),
-  dashboardSidebar(HTML(paste("Enter a value between 1 and", n_rows)),
-                   
-                   selectInput(inputId = "JobRole",
-                               label = "Select Job role:",
-                               choices = all_Jobroles,
-                               selected = "Research Scientist",
-                               selectize = TRUE,
-                               multiple =FALSE
-                   ),
-                   
-                  
-                   
-                   # Select variable for y-axis
-                   selectInput(
-                     inputId = "y",
-                     label = "Y-axis:",
-                     choices = c(
-                       "Age" = "Age",
-                       "Attrition" = "Attrition",
-                       "Gende" = "Gender",
-                       "Job Satisfaction" = "JobSatisfaction",
-                       "Monthly Income" = "MonthlyIncome",
-                       "Distance From Home" = "DistanceFromHome"
-                     ),
-                     selected = "Age"
-                   ),
-                   # Select variable for x-axis
-                   selectInput(
-                     inputId = "x",
-                     label = "X-axis:",
-                     choices = c(
-                       "Age" = "Age",
-                       "Attrition" = "Attrition",
-                       "Gende" = "Gender",
-                       "Job Satisfaction" = "JobSatisfaction",
-                       "Monthly Income" = "MonthlyIncome",
-                       "Distance From Home" = "DistanceFromHome"
-                     ),
-                     selected = "MonthlyIncome"
-                   ),
-                   selectInput(
-                     inputId = "z",
-                     label = "Color by:",
-                     choices = c(
-                       "Age" = "Age",
-                       "Attrition" = "Attrition",
-                       "Gender" = "Gender",
-                       "Job Satisfaction" = "JobSatisfaction",
-                       "Monthly Income" = "MonthlyIncome",
-                       "Distance From Home" = "DistanceFromHome"
-                     ),
-                     selected = "Attrition"
-                   ),
-                   # Set alpha level for transparency plots
-                   sliderInput(
-                     inputId = "alpha",
-                     label = "Alpha:",
-                     min = 0,
-                     max = 1,
-                     value = 0.5
-                   )),
-  dashboardBody(  tabBox(
-    title = "Scatterplot ",
-    # The id lets us use input$tabset1 on the server to find the current tab
-    id = "tabset1", height = "450px",
+  dashboardSidebar(
+    HTML(paste("Enter a value between 1 and", n_rows)),
+    
+    selectInput(
+      inputId = "JobRole",
+      label = "Select Job role:",
+      choices = all_Jobroles,
+      selected = "Research Scientist",
+      selectize = TRUE,
+      multiple = FALSE
+    ),
+    
+    
+    
+    # Select variable for y-axis
+    selectInput(
+      inputId = "y",
+      label = "Y-axis:",
+      choices = c(
+        "Age" = "Age",
+        "Attrition" = "Attrition",
+        "Gende" = "Gender",
+        "Job Satisfaction" = "JobSatisfaction",
+        "Monthly Income" = "MonthlyIncome",
+        "Distance From Home" = "DistanceFromHome",
+        "Job Roles" = "JobRole"
+      ),
+      selected = "Age"
+    ),
+    # Select variable for x-axis
+    selectInput(
+      inputId = "x",
+      label = "X-axis:",
+      choices = c(
+        "Age" = "Age",
+        "Attrition" = "Attrition",
+        "Gende" = "Gender",
+        "Job Satisfaction" = "JobSatisfaction",
+        "Monthly Income" = "MonthlyIncome",
+        "Distance From Home" = "DistanceFromHome",
+        "Job Roles" = "JobRole"
+      ),
+      selected = "MonthlyIncome"
+    ),
+    selectInput(
+      inputId = "z",
+      label = "Color by:",
+      choices = c(
+        "Age" = "Age",
+        "Attrition" = "Attrition",
+        "Gender" = "Gender",
+        "Job Satisfaction" = "JobSatisfaction",
+        "Monthly Income" = "MonthlyIncome",
+        "Distance From Home" = "DistanceFromHome"
+      ),
+      selected = "Attrition"
+    ),
+    # Set alpha level for transparency plots
+    sliderInput(
+      inputId = "alpha",
+      label = "Alpha:",
+      min = 0,
+      max = 1,
+      value = 0.5
+    )
+  ),
+ 
   
-    tabPanel("Tab1", "First tab content",  plotOutput(outputId = "scatterplot")),
-    tabPanel("Tab2", "Tab content 2",plotOutput(outputId = "densityplot", height = 350)),
-    tabPanel("Tab3", "First tab content", DT::dataTableOutput(outputId = "Attritiontable", height = 350) ), width = 12)
+  dashboardBody(
+    tabBox(
+      title = "Scatterplot ",
+      # The id lets us use input$tabset1 on the server to find the current tab
+      id = "tabset1",
+      height = 550,
+      
+      tabPanel(
+        "Plot 1",
+        "First tab content",
+        plotOutput(outputId = "scatterplot"),
+        plotOutput(outputId = "densityplot", height = 250)
+      ),
+      tabPanel(
+        "Plot2",
+        "Tab content 2",
+        htmlOutput("bubble", height = 250),
+        
+        plotlyOutput("plot1", height = 250)
+       
+      ),
+      tabPanel(
+        "Dataset Representation",
+        "First tab content",
+        DT::dataTableOutput(outputId = "Attritiontable", height = 250)
+      ),
+      width = 12
+    )
   )
   
-    
-    
-               )
-
   
+  
+)
+
+
 
 
 # Define server function required to create the scatterplot
 server <- function(input, output) {
-  
   dataInput <- reactive({
-    filter(emp,emp$JobRole==input$JobRole)
+    filter(emp, emp$JobRole == input$JobRole)
   })
   
   # Create scatterplot object the plotOutput function is expecting
@@ -137,9 +166,7 @@ server <- function(input, output) {
   })
   
   output$densityplot <- renderPlot({
-    ggplot(data = emp, aes_string(
-      x = input$x
-    )) +
+    ggplot(data = emp, aes_string(x = input$x)) +
       geom_density()
   })
   
@@ -147,6 +174,32 @@ server <- function(input, output) {
   output$Attritiontable <- DT::renderDataTable({
     dataInput()
   })
+  
+  
+  
+  output$bubble <- renderGvis({
+    
+    gvisBubbleChart(a, idvar="Department",
+                    xvar="Age", yvar="MonthlyIncome",
+                    colorvar="JobRole", sizevar="DistanceFromHome",
+                    options=list(title = "Bubble Chart according to the department", height = 350, width = 1300
+                    )
+    ) })
+  
+  output$plot1 <- renderPlotly(
+    {abcd <- emp$input$x
+    abcd1 <- emp$input$y
+    plot_ly(emp, x = ~get(input$x), y = ~get(input$y), xaxis = list(title = input$x),
+            yaxis = list(title = input$y) )
+   
+  })
+  
+  output$event <- renderPrint({
+    d <- event_data("plotly_hover")
+    if (is.null(d)) "Hover on a point!" else d
+  })
+  
+  
   
   
 }
